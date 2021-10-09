a195bfb9b33f7114005e4663118b56a9d7c85b04
soon-404 <tfpsoon@gmail.com>
2021-10-09 16:50:06 +0700

    task 1

--- Lab_Design_Pattern/SoftArchDesign/src/main/java/creational/JSONBookMetadataFormatter.java
+++ Lab_Design_Pattern/SoftArchDesign/src/main/java/creational/JSONBookMetadataFormatter.java
@@ -1,22 +1,52 @@
 package creational;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class JSONBookMetadataFormatter implements BookMetadataFormatter {
 
-    @Override
+    private JSONObject bookObj;
+    private JSONArray bookArray;
+    private JSONObject bookInnerObj;
+    private JSONArray authorArray;
+
+    public JSONBookMetadataFormatter(){
+        reset();
+    }
+
     public BookMetadataFormatter reset() {
-        // Please implement this method. You may create additional methods as you see fit.
-        return null;
+        try {
+            bookObj = new JSONObject();
+            bookArray = new JSONArray();
+
+            bookObj.put(Book.class.getSimpleName() + "s", bookArray);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this;
     }
 
     @Override
     public BookMetadataFormatter append(Book b) {
-        // Please implement this method. You may create additional methods as you see fit.
+        try {
+            authorArray = new JSONArray();
+            bookInnerObj = new JSONObject();
+            bookInnerObj.put(Book.Metadata.ISBN.value, b.getISBN());
+            for (String author : b.getAuthors()) {
+                authorArray.add(author);
+            }
+            bookInnerObj.put(Book.Metadata.AUTHORS.value, authorArray);
+            bookInnerObj.put(Book.Metadata.TITLE.value, b.getTitle());
+            bookInnerObj.put(Book.Metadata.PUBLISHER.value, b.getPublisher());
+            bookArray.add(bookInnerObj);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         return this;
     }
 
     @Override
     public String getMetadataString() {
-        // Please implement this method. You may create additional methods as you see fit.
-        return null;
+        return bookObj.toJSONString();
     }
 }
